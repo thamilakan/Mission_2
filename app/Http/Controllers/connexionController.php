2668ab7d641794dc6835d01447b0d9f13995f75a
DHARMAPALAN Thamilakan <92038820+thamilakan@users.noreply.github.com>
2023-03-29 12:40:17 +0200

    Add files via upload

---
+++ app/Http/Controllers/connexionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use PdoGsb;
+
+class connexionController extends Controller
+{
+    function connecter(){
+        
+        return view('connexion')->with('erreurs',null);
+    }
+
+    function valider(Request $request){
+        $login = $request['login'];
+        $mdp = $request['mdp'];
+        $visiteur = PdoGsb::getInfosVisiteur($login,$mdp);
+      
+        if(!is_array($visiteur))
+        {
+            $compta =PdoGsb::getInfosCompta($login,$mdp);
+            if(!is_array($compta))
+            {
+                $erreurs[] = "Login ou mot de passe incorrect(s)";
+                return view('connexion')->with('erreurs',$erreurs);
+            }
+          
+            else
+            {
+                session(['comptable' => $compta]);
+                return view('sommaireCompta')->with('comptable',session('comptable'));
+            }
+        }
+        else{
+            session(['visiteur' => $visiteur]);
+            return view('sommaire')->with('visiteur',session('visiteur'));
+        }            
+    }
+
+    function deconnecter(){
+            session(['visiteur' => null]);
+            return redirect()->route('chemin_connexion');
+       
+           
+}
+}
+       
